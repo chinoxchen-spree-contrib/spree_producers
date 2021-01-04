9ae20da23cb77d2a953416178e49d7e6cf46b248
rmartinezapp <robert.martinez.app@gmail.com>
2021-01-04 03:03:47 -0300

    add scope in_stock on product decorator

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,10 @@
 module Spree::ProductDecorator
   Spree::Product.whitelisted_ransackable_associations += %w[producer]
 
+  def self.prepended(base)
+    base.scope :in_stock, -> { joins(master: :prices, variants: :stock_items).where("#{Spree::StockItem.quoted_table_name}.count_on_hand > 0 AND #{Spree::Product.quoted_table_name}.available_on <= ?", Time.now).uniq }
+  end
+
   def local?
     producer.local?
   rescue
