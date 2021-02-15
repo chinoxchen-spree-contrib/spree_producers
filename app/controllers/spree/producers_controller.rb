c887aa1fb432cd664a01b788c6e64124cd6a0450
chinoxchen <chienfu.udp@gmail.com>
2021-02-14 23:12:57 -0300

    add producers paginator

--- app/controllers/spree/producers_controller.rb
+++ app/controllers/spree/producers_controller.rb
@@ -1,21 +1,24 @@
 module Spree
   class ProducersController < Spree::StoreController
-    before_action :load_producers, only: [:index, :show]
     before_action :load_producer, only: [:show]
 
     respond_to :html
 
     def index
-      @producers = Spree::Producer.all.active
+      page = if params[:page].respond_to?(:to_i)
+        params[:page].to_i <= 0 ? 1 : params[:page].to_i
+      else
+        1
+      end
+
+      curr_page = page || 1
+      per_page = 16
+      @producers = Spree::Producer.all.active.page(curr_page).per(per_page)
     end
 
     def show; end
 
     private
-
-    def load_producers
-      @producers = Spree::Producer.all.active
-    end
 
     def load_producer
       @producer = @producers.friendly.find(params[:id])
