e8dd1f30063f3fd7455449b655f3354baa19e121
chinoxchen <chienfu.udp@gmail.com>
2021-04-07 23:13:10 -0400

    change file key

--- app/models/spree/producer_image.rb
+++ app/models/spree/producer_image.rb
@@ -3,6 +3,13 @@ module Spree
     include Spree::Image::Configuration::ActiveStorage
     include Rails.application.routes.url_helpers
 
+    before_save :change_key
+
+    def change_key
+      return if self.attachment.key.split('/').length == 2
+      self.attachment.key = "#{attachment.key}.#{attachment.content_type.split('/').second}"
+    end
+
     def styles
       self.class.styles.map do |_, size|
         width, height = size[/(\d+)x(\d+)/].split('x')
